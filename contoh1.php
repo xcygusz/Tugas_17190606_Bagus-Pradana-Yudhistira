17325a474aeeb5887011773b6910ff2dc3764942
Bagus Pradana <101781332+xcygusz@users.noreply.github.com>
2022-04-28 14:01:03 +0700

    Create contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h> Perkenalkan </h> <br>";
+        echo " Nama Saya Bagus Pradana <br>
+        Saya lahir di Jakarta 29 september 2000 <br>
+        Saya tinggal di Pamulang";
+    }
+} 
